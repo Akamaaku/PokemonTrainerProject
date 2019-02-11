bcdb33791cf71029a64dc21fad1585823a20d99f
Mark Nunez <marknunez917@gmail.com>
2019-02-11 09:50:03 -0600

    Added validations and the relationships between the models element_type, pokemon, teams, trainers

--- app/models/element_type.rb
+++ app/models/element_type.rb
@@ -1,3 +1,4 @@
 class ElementType < ApplicationRecord
+    has_many :pokemons
     validates :typeName, presence: true, uniqueness: true
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,7 @@
 class Pokemon < ApplicationRecord
   belongs_to :generation
   belongs_to :element_type
+  has_many :team_members
   validates :name, :pokedexID, presence: true
   validates :pokedexID, numericality: { only_integer: true }
 end
