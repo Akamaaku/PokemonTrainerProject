5e20a5eefe8d1c964fa8439b63529549d215b0b0
Mark Nunez <marknunez917@gmail.com>
2019-02-12 10:31:16 -0600

    Created Page model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_150343) do
+ActiveRecord::Schema.define(version: 2019_02_12_163056) do
 
   create_table "element_types", force: :cascade do |t|
     t.string "typeName"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2019_02_12_150343) do
 
   create_table "generations", force: :cascade do |t|
     t.string "generation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.string "permalink"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
