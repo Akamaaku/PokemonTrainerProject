f044b61338f808fa54209ef7aafb1421272af49a
Mark Nunez <marknunez917@gmail.com>
2019-02-11 20:25:15 -0600

    Added new column to games model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_012937) do
+ActiveRecord::Schema.define(version: 2019_02_12_022437) do
 
   create_table "element_types", force: :cascade do |t|
     t.string "typeName"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_012937) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "detailsURL"
   end
 
   create_table "generations", force: :cascade do |t|
