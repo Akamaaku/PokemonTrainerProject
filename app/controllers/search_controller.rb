f5bc83ac9ca50e14832486969e898c4a663a1966
Mark Nunez <marknunez917@gmail.com>
2019-02-13 00:58:12 -0600

    added search controller and code to add it to the menu bar.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def results
+    @query = params[:input]
+    @trainers = Trainer.where('name LIKE ?', "%#{@query}%")
+    @team = Team.where('teamName LIKE ?', "%#{@query}%")
+    @team_members = TeamMember.where('nickname LIKE ?', "%#{@query}%")
+    @pokemons = Trainer.where('name LIKE ?', "%#{@query}%")
+    @generations = Trainer.where('generation LIKE ?', "%#{@query}%")
+    @games = Trainer.where('title LIKE ?', "%#{@query}%")
   end
 end
